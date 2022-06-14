522ac2d5988b77d7709e3147c5771fffd0abe60d
minutmaidman <104789809+minutmaidman@users.noreply.github.com>
2022-06-14 19:22:47 -0400

    all components in
    je commences a route les components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import {BrowserRouter as Router, Route} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './components/navbar/navbar';
 import Liste from './components/list/joblist';
+import Dashboard from './components/dashboard/dashboard';
+import Register from './components/Register';
+import Login from './components/Login';
 //import Ajout from './components/ajout/ajout';
 //import Logo from './components/logo/logo';
 import { Navigate } from "react-router-dom"; 
@@ -14,11 +17,8 @@ class App extends React.Component {
       <Router>
 
         <Route exact path="/">
-          <Navigate to="/joblist" />
+          <Dashboard/>
         </Route>
-
-        <Navbar/>
-        <Route path= "/liste" component={Liste} />
       </Router>
     </div>
   );
